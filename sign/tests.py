939c283777060b7deadb43d900ae8df373f3c75a
changdig <changdig@foxmail.com>
2020-05-26 16:13:24 +0800

    Add ModuleTest

--- sign/tests.py
+++ sign/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from sign.models import Event,Guest
+from django.utils import timezone
 
 # Create your tests here.
+class ModuleTest(TestCase):
+
+    def setUp(self):
+        Event.objects.create(id=1, name="Apple macbookpro event", status=True,
+                            limit=2000,
+                            address="hangzhou", 
+                            start_time=timezone.now())
+        Guest.objects.create(id=1, event_id=1, realname="Gui",
+                            phone="12345678912",
+                            email="123@123.com",
+                            sign=False)
+    
+    def test_event_models(self):
+        result = Event.objects.get(name="Apple macbookpro event")
+        self.assertEqual(result.address, "hangzhou")
+        self.assertTrue(result.status)
+    
+    def test_guest_models(self):
+        result = Guest.objects.get(phone="12345678912")
+        self.assertEqual(result.realname, "Gui")
+        self.assertFalse(result.sign)
+
+    def tearDown(self):
+        pass
