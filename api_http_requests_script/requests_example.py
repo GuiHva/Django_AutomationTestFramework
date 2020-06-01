81d54359cf5910e970c67e247c4ff04021f4a5f1
Changdi Gui <changdig@foxmail.com>
2020-06-01 11:39:49 +0800

    Update requests example

--- api_http_requests_script/requests_example.py
+++ api_http_requests_script/requests_example.py
@@ -1,16 +1,20 @@
 import requests
+import json
 
 '''
-Use requests to simulate HTTP requests to get API data
+Use requests to simulate HTTP requests to get API data, use token
 
 API:
-    /api/consumable/lists
+    /api/registration/report/lists
 Host:
-    http://127.0.0.1:8000
+    http://192.168.1.91:20000
 '''
 
-Host = 'http://127.0.0.1:8000'
-API_path = '/api/get_event_list/'
-r = requests.get(Host + API_path, auth = ('test','test123456'), params={'eid':'1'})
+Host = 'http://192.168.1.91:20000'
+API_path = '/api/registration/report/lists'
+headers = {'Authorization': 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xOTIuMTY4LjEuOTE6MjAwMDBcL2FwaVwvYXV0aFwvbG9naW4iLCJpYXQiOjE1OTA5Nzk0MDMsImV4cCI6MTU5MTU4NDIwMywibmJmIjoxNTkwOTc5NDAzLCJqdGkiOiJzUUphRmdvNFlmNVBwNEJqIiwic3ViIjo1MiwicHJ2IjoiODdlMGFmMWVmOWZkMTU4MTJmZGVjOTcxNTNhMTRlMGIwNDc1NDZhYSJ9.d31FmkfP0h4bf3uaYs5TcVKeZkTD4u6TpnLqza2pz8I'}
+r = requests.get(Host + API_path, headers = headers, params={'page_size':'10', 'print_status': '1'})
 result = r.json()
-print(result)
+with open('result.json', 'w', encoding='utf-8') as f:
+    json.dump(result, f)
+    print('Done!')
